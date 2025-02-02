dfd9f1a5ed2b777dacdb1a44c5d2c57131143c74
dima <flyvlogpl@gmail.com>
2025-02-02 21:39:53 +0300

    Add README.md

---
+++ README.md
@@ -0,0 +1,37 @@
+## Установка
+
+```bash
+git clone https://github.com/dima-doroshenko/fastapi-jwt-auth-example
+cd fatsapi-jwt-auth-example
+pip install -r requirements.txt
+```
+
+### .env
+
+В файле .env должны быть ключи **`EMAIL_LOGIN`** и **`EMAIL_PASSWORD`**, в которых содержатся почта и пароль от аккаунта, который будет отправлять сообщения для верификации
+
+### JWT
+
+Сгенерируйте ключи для созданий JWT, поместите их по адресу **`certs\jwt-public.pem`** и **`certs\jwt-private.pem`**
+
+### Тесты
+
+Для проверки работы запустите тесты
+```bash
+pytest -v -s -c tests\pytest.ini
+```
+
+## Запуск приложения
+
+### Через `uvicorn`
+
+```bash
+uvicorn src.main:app --reload
+```
+
+### Через `Docker`
+
+
+```bash
+docker compose up --build
+```
--- src/config.py
+++ src/config.py
@@ -11,6 +11,7 @@ BASEDIR = Path(__file__).parent.parent
 
 env = dotenv.dotenv_values(str(BASEDIR / ".env"))
 
+
 class Email(BaseModel):
     server: str = "smtp.gmail.com"
     port: int = 587
@@ -35,9 +36,11 @@ class DBSettings(BaseModel):
     url: str = "sqlite+aiosqlite:///database.db"
     test_url: str = url[:-3] + "_test.db"
 
+
 class App(BaseModel):
     name: str = "FastAPI JWT Auth"
     debug: bool = True
+
 
 class Settings(BaseSettings):
     db: DBSettings = DBSettings()
@@ -47,12 +50,12 @@ class Settings(BaseSettings):
     app: App = App()
 
     def debug_decorator(self, func):
-        '''Если debug == True, функция не выполнится'''
+        """Если debug == True, функция не выполнится"""
 
         def wrapper(*args, **kwgs) -> Any | None:
             if not self.app.debug:
                 return func(*args, **kwgs)
-                
+
         return wrapper
 
 
